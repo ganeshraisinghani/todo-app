60c8b1e78f1479e1ad8ed81e01cab916866faf95
ganesh <ganeshmohan8297@gmail.com>
2025-06-06 14:03:28 +0530

    Models/Todo.js file Commit

---
+++ models/Todo.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean
+}, { timestamps: true });
+
+module.exports = mongoose.model('Todo', todoSchema);
